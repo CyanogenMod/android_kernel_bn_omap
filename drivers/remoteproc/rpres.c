87993fdf936e2af612240de337c4250c3eb1c77f
Artur Polivenko <x0199365@ti.com>
2013-06-26 16:42:35 +0300

    fix a bug constraint scale and bandwidth (IVA)
    This issue reproduced on next steps:
    1. Start AV playback
    2. Perform Suspend/Resume
    
    Actual Result:
    "error setting constraint scale" and "error bandwidth constraint" errors occur (logs are attached).
    
    This issue to arises in next situation:
    1. vidio is playing.
    2. call function early_suspend (push suspend button)
    handler function for abe_early_suspend entered in the dpll_cascading,
    all condition are met for entrance to dpll_cascading is:
     system is in early suspend (screen is off)
     single stream is active and is LP (ping-pong)
     OPP is 50 or less (DL1 path only)
    3. vidio stops (do not exit from the dpll_cascading )
    4. invokes function rpres_set_constraints:
     call omap_device_scale which returns EBUSY, if dpll_cascading has status enabled.
    5. print errors
    
     drivers/remoteproc/rpres.c |    4 ++++
     1 file changed, 4 insertions(+)
    
    Change-Id: I9c3afbde8d4ed0ac6fe77d04d1eee6f1ca44da4a
    Signed-off-by: Artur Polivenko <x0199365@ti.com>

--- drivers/remoteproc/rpres.c
+++ drivers/remoteproc/rpres.c
@@ -18,6 +18,7 @@
 #include <linux/opp.h>
 #include <plat/omap_device.h>
 #include <plat/rpres.h>
+#include <mach/omap4-common.h>
 
 static LIST_HEAD(rpres_list);
 static DEFINE_SPINLOCK(rpres_lock);
@@ -44,6 +45,7 @@ struct rpres *rpres_get(const char *name)
 	if (!r)
 		return ERR_PTR(-ENOENT);
 
+	omap4_dpll_cascading_blocker_hold(&r->pdev->dev);
 	mutex_lock(&r->lock);
 	if (r->state == RPRES_ACTIVE) {
 		pr_err("%s:resource already active\n", __func__);
@@ -73,6 +75,7 @@ void rpres_put(struct rpres *obj)
 		obj->state = RPRES_INACTIVE;
 	}
 	mutex_unlock(&obj->lock);
+	omap4_dpll_cascading_blocker_release(&obj->pdev->dev);
 }
 EXPORT_SYMBOL(rpres_put);
 
